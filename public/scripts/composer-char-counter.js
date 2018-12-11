40ed852c49d4ff18e699ea943cd421ca5d537b76
Richmond Wong <richmondwong@gmail.com>
2018-12-11 23:34:47 +0000

    Added Jquery character counter and updated CSS

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('textarea#userInput').on("keypress", function(){
+    var $textarea = $(this)
+    var characterCount = $textarea.val().length
+    var $counter = $textarea.siblings(".counter").text(140 - characterCount)
+
+    if (characterCount > 140){
+      $textarea.siblings(".counter").css("color", "red")
+    }
+    console.log("This is length:", 140 - tweetStr)
+    console.log("charCount test", charCount)
+  })
+});
+
--- server/index.js
+++ server/index.js
@@ -26,8 +26,15 @@ const DataHelpers = require("./lib/data-helpers.js")(db);
 // so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
+// app.post('/tweets/', (req, res) => {
+//   res.redirect('/tweets')
+// })
+
+
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
