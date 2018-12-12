09a296383be6cf9314ed745acf8cfeca5798e048
Richmond Wong <richmondwong@gmail.com>
2018-12-12 21:47:00 +0000

    Added ability to dynamically generate tweets via renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,88 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+// Need to change section to article?
+// let $tweet = $('<article>').addClass('tweet')
 
 
-
-function createTweetElement(input){
-var appendedHTML =
- ` <article class="fresh-tweet-box">
-    <header class="fresh-tweet-header">
-      <img src="" class="tweet-photos">
-      <div class="enclose-name-handle">
-        <span class="tweet-owner-box">${input["user"]["name"]}</span>
-        <span class="handle-name-box">${input["user"]["handle"]}</span>
-    </div>
-    </header>
-      <span class="actual-tweet"> ${input["content"]["text"]}</span>
-    <footer class="fresh-tweet-footer">
-      <span class="tweet-days"> ${input["created_at"]} </span>
-  <span class="icons-box">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </span>
-    </footer>
-  </article>`
-return appendedHTML
-}
 
 $(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
-var $tweet = createTweetElement(tweetData);
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.overall-tweets-box').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  function renderTweets(input){
+    input.forEach(function(individualUsers){
+      var individualTweet = createTweetElement(individualUsers);
+      $('.overall-tweets-box').append(individualTweet)
+    })
+  }
+
+  function createTweetElement(input){
+    var tweet =
+     ` <article class="fresh-tweet-box">
+        <header class="fresh-tweet-header">
+          <img src="" class="tweet-photos">
+          <div class="enclose-name-handle">
+            <span class="tweet-owner-box">${input["user"]["name"]}</span>
+            <span class="handle-name-box">${input["user"]["handle"]}</span>
+        </div>
+        </header>
+          <span class="actual-tweet"> ${input["content"]["text"]}</span>
+        <footer class="fresh-tweet-footer">
+          <span class="tweet-days"> ${input["created_at"]} </span>
+      <span class="icons-box">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+        </footer>
+      </article>`
+
+    return tweet;
+  }
+
+  renderTweets(data)
 })
